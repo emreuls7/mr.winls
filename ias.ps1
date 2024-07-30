bb4e85b25e03cb7c6b522ae027285fdb77ea5d2d
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-07-30 21:28:06 +0300

    Update ias.ps1

--- ias.ps1
+++ ias.ps1
@@ -4,12 +4,12 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/WindowsAddict/IDM-Activation-Script/main/IAS.cmd'
-$DownloadURL2 = 'https://bitbucket.org/WindowsAddict/idm-activation-script/raw/main/IAS.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/emreuls7/mr.winls/tool/ias.cmd'
+$DownloadURL2 = 'https://raw.githubusercontent.com/emreuls7/mr.winls/tool/ias.cmd'
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\IAS_$rand.cmd" } else { "$env:TEMP\IAS_$rand.cmd" }
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\IAS.cmd" } else { "$env:TEMP\IAS.cmd" }
 
 try {
     $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
@@ -25,5 +25,5 @@ Set-Content -Path $FilePath -Value $content
 
 Start-Process $FilePath $ScriptArgs -Wait
 
-$FilePaths = @("$env:TEMP\IAS*.cmd", "$env:SystemRoot\Temp\IAS*.cmd")
+$FilePaths = @("$env:TEMP\IAS.cmd", "$env:SystemRoot\Temp\IAS.cmd")
 foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
