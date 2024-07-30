3aec8d8d1bce095ed0b7a3cbeb8ad0da40ccd0c3
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-07-30 21:25:29 +0300

    Update mas.ps1

--- mas.ps1
+++ mas.ps1
@@ -1,17 +1,32 @@
-            # Güvenlik protokolünü TLS 1.2 olarak ayarla
-            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# Enable TLSv1.2 for compatibility with older clients for current session
+[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
 
-            # Script'in URL'sini tanımla
-            $scriptUrl = "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/0884271c4fcdc72d95bce7c5c7bdf77ef4a9bcef/MAS/All-In-One-Version/MAS_AIO-CRC32_31F7FD1E.cmd"
+$DownloadURL1 = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/0884271c4fcdc72d95bce7c5c7bdf77ef4a9bcef/MAS/All-In-One-Version/MAS_AIO-CRC32_31F7FD1E.cmd'
+$DownloadURL2 = 'https://bitbucket.org/WindowsAddict/microsoft-activation-scripts/raw/0884271c4fcdc72d95bce7c5c7bdf77ef4a9bcef/MAS/All-In-One-Version/MAS_AIO-CRC32_31F7FD1E.cmd'
 
-            # Script'in kaydedileceği yolu tanımla
-            $scriptPath = "$env:TEMP\MAS_AIO-CRC32_31F7FD1E.cmd"
+$URLs = @($DownloadURL1, $DownloadURL2)
+$RandomURL1 = Get-Random -InputObject $URLs
+$RandomURL2 = $URLs -ne $RandomURL1
 
-            # Script'i indir
-            Invoke-WebRequest -Uri $scriptUrl -OutFile $scriptPath
+try {
+    $response = Invoke-WebRequest -Uri $RandomURL1 -UseBasicParsing
+}
+catch {
+	$response = Invoke-WebRequest -Uri $RandomURL2 -UseBasicParsing
+}
 
-            # İsteğe bağlı olarak indirilen script'in içeriğini görüntüle
-            # Get-Content $scriptPath | Out-Host
+$rand = [Guid]::NewGuid().Guid
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
 
-            # Script'i çalıştır
-            Invoke-Expression -Command "& `$scriptPath"
+$ScriptArgs = "$args "
+$prefix = "@::: $rand `r`n"
+$content = $prefix + $response
+Set-Content -Path $FilePath -Value $content
+
+# Set ComSpec variable for current session in case its corrupt in the system
+$env:ComSpec = "$env:SystemRoot\system32\cmd.exe"
+Start-Process cmd.exe "/c """"$FilePath"" $ScriptArgs""" -Wait
+
+$FilePaths = @("$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
