923be8bc68ee4a8a6862484df3fe97f47e9c37fa
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-07-30 22:09:05 +0300

    Update menu05.cmd

--- menu05.cmd
+++ menu05.cmd
@@ -2,38 +2,38 @@
 REM Menu
 :menu5
 cls
-echo ------------------------------------------------------------------------------------------------------------------------
-echo --- Menu5                                  --- Driver Installer ---                                CREATED BY MRLSx7 ---
-echo ------------------------------------------------------------------------------------------------------------------------
-echo [1]  Driver Booster                                                                                                
-echo [2]  NVIDIA GeForce Experience                                                                                     
-echo [3]  NVIDIA GeForce NOW                                                                                            
-echo [4]  NVIDIA PhysX System Software                                                                                  
-echo [5]  NVIDIA Broadcast                                                                                              
-echo [6]  NVIDIA CUDA Toolkit                                                                                           
-echo [7]  Intel Driver Support Assistant                                                                                
-echo [8]  spacedesk Windows DRIVER                                                                                      
-echo [9]  spacedesk Windows VIEWER                                                                                      
-echo [10] DisplayFusion                                                                                                 
-echo ------------------------------------------------------------------------------------------------------------------------
+echo ----------------------------------------------------------
+echo --- Menu5          ---  ---          CREATED BY MRLSx7 ---
+echo ----------------------------------------------------------
+echo [1]
+echo [2]
+echo [3]
+echo [4]
+echo [5]
+echo [6]
+echo [7]
+echo [8]
+echo [9]
+echo [10]
+echo ----------------------------------------------------------
 echo [0] Exit                                                   
-echo ------------------------------------------------------------------------------------------------------------------------
+echo ----------------------------------------------------------
 ::---------------------------------------------------------------------------------------------------------------------------
 set /p choice="Enter your choice (0,1,2,3...): "
 ::---------------------------------------------------------------------------------------------------------------------------
-if "%choice%"=="1" winget install IObit.DriverBooster -e
-if "%choice%"=="2" winget install Nvidia.GeForceExperience -e
-if "%choice%"=="3" winget install Nvidia.GeForceNow -e
-if "%choice%"=="4" winget install Nvidia.PhysX -e
-if "%choice%"=="5" winget install Nvidia.Broadcast -e
-if "%choice%"=="6" winget install Nvidia.CUDA -e
-if "%choice%"=="7" winget install Intel.IntelDriverAndSupportAssistant -e
-if "%choice%"=="8" winget install Datronicsoft.SpacedeskDriver.Server-e
-if "%choice%"=="9" winget install Datronicsoft.SpacedeskDriver.Client -e
-if "%choice%"=="10" winget install BinaryFortress.DisplayFusion -e
+if "%choice%"=="1" 
+if "%choice%"=="2" 
+if "%choice%"=="3" 
+if "%choice%"=="4" 
+if "%choice%"=="5" 
+if "%choice%"=="6" 
+if "%choice%"=="7" 
+if "%choice%"=="8" 
+if "%choice%"=="9" 
+if "%choice%"=="10" 
 ::---------------------------------------------------------------------------------------------------------------------------
 if "%choice%"=="0" exit
 ::---------------------------------------------------------------------------------------------------------------------------
-pause
+::pause
 goto menu5
 ::---------------------------------------------------------------------------------------------------------------------------
