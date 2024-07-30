d701ae1c230e5d73e8b714dfd511880baccfad5b
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-07-31 00:39:00 +0300

    Update menu01.cmd

--- menu01.cmd
+++ menu01.cmd
@@ -78,7 +78,7 @@ if "%choice%"=="51" robocopy "\\192.168.18.2\setup\source\key-crack+\obs-telepor
 if "%choice%"=="52" robocopy "\\192.168.18.2\setup\source\setup\AnyToISO\AnyToISOcrack" "C:\Program Files (x86)\AnyToISO" anytoiso.exe /IS || echo Copy failed for AnyToISO Key
 if "%choice%"=="53" robocopy "\\192.168.18.2\setup\source\key-crack+\Revo_Uninstaller_Pro" "C:\ProgramData\VS Revo Group\Revo Uninstaller Pro" revouninstallerpro5.lic /IS || echo Copy failed for Revo Uninstaller Pro Key
 if "%choice%"=="54" robocopy "\\192.168.18.2\setup\source\key-crack+\AOMEI_Partition_Assistant_Pro" "C:\Program Files (x86)\AOMEI Partition Assistant" cfg.ini /IS || echo Copy failed for AOMEI Partition Assistant Pro Key
-if "%choice%"=="55" powershell -command "irm https://massgrave.dev/ias | iex" || echo Activation script failed
+if "%choice%"=="55" powershell -command "https://raw.githubusercontent.com/emreuls7/mr.winls/tool/massgrave_ias.ps1" || echo Activation script failed
 
 if "%choice%"=="0" exit
 
