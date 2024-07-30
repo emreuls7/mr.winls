1773fbb09fde7c28bb1a724821d7f8efb185bbb4
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-07-30 21:32:34 +0300

    Update mas.ps1

--- mas.ps1
+++ mas.ps1
@@ -1,56 +1,32 @@
-# Daha eski istemcilerle uyumluluk için TLSv1.2'yi etkinleştir
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# Enable TLSv1.2 for compatibility with older clients for current session
+[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
 
-# İndirilecek URL'ler
 $DownloadURL1 = 'https://raw.githubusercontent.com/emreuls7/mr.winls/tool/mas.cmd'
 $DownloadURL2 = 'https://raw.githubusercontent.com/emreuls7/mr.winls/tool/mas.cmd'
 
 $URLs = @($DownloadURL1, $DownloadURL2)
 $RandomURL1 = Get-Random -InputObject $URLs
-$RandomURL2 = $URLs | Where-Object { $_ -ne $RandomURL1 }
-
-$response = $null
+$RandomURL2 = $URLs -ne $RandomURL1
 
 try {
-    # İlk URL'den script'i indir
     $response = Invoke-WebRequest -Uri $RandomURL1 -UseBasicParsing
 }
 catch {
-    try {
-        # İlk URL başarısız olursa, ikinci URL'den script'i indir
-        $response = Invoke-WebRequest -Uri $RandomURL2 -UseBasicParsing
-    }
-    catch {
-        Write-Error "Her iki URL'den de script indirilemedi."
-        exit 1
-    }
+	$response = Invoke-WebRequest -Uri $RandomURL2 -UseBasicParsing
 }
 
-# Benzersiz bir GUID oluştur
 $rand = [Guid]::NewGuid().Guid
-
-# Yönetici olup olmadığını kontrol et
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
 
-# Dosya yolunu belirle
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS.cmd" } else { "$env:TEMP\MAS.cmd" }
-
-# Script argümanlarını ve içeriği ayarla
 $ScriptArgs = "$args "
 $prefix = "@::: $rand `r`n"
-$content = $prefix + $response.Content
-
-# İçeriği dosyaya yaz
+$content = $prefix + $response
 Set-Content -Path $FilePath -Value $content
 
-# Geçici dosyayı çalıştır
+# Set ComSpec variable for current session in case its corrupt in the system
 $env:ComSpec = "$env:SystemRoot\system32\cmd.exe"
 Start-Process cmd.exe "/c """"$FilePath"" $ScriptArgs""" -Wait
 
-# Geçici dosyaları temizle
-$FilePaths = @("$env:TEMP\MAS.cmd", "$env:SystemRoot\Temp\MAS.cmd")
-foreach ($FilePath in $FilePaths) {
-    if (Test-Path $FilePath) {
-        Remove-Item -Path $FilePath -Force
-    }
-}
+$FilePaths = @("$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
