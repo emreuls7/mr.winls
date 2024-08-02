e337f57030f4345d6416d243c03c129f24d2960c
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-02 21:56:12 +0300

    Update program.ps1

--- program.ps1
+++ program.ps1
@@ -5,19 +5,19 @@ function Show-Menu {
     Write-Host "------------------------------------------------------------------------------------------------------------------------" -ForegroundColor Green
     Write-Host "--- Main Menu                  --- Welcome to Software Install ---                                 CREATED BY MRLSx7 ---" -ForegroundColor Red
     Write-Host "------------------------------------------------------------------------------------------------------------------------" -ForegroundColor Green
-    Write-Host "[1] * Program Install *                            [6]  Web Browser Install               " -ForegroundColor DarkGray
+    Write-Host "[1] Program Install *                              [6]  Web Browser Install               " -ForegroundColor DarkGray
     Write-Host "[2] Winget 1* Install                              [7]  VPN + Remote Install              " -ForegroundColor DarkGray
     Write-Host "[3] Winget 2* Install                              [8]  Media Player Install              " -ForegroundColor DarkGray
     Write-Host "[4] Choco Instal                                   [9]  Chat Messenger Install            " -ForegroundColor DarkGray
     Write-Host "[5] MS Store Install                               [10] Gaming Launcher Install           " -ForegroundColor DarkGray
     Write-Host "------------------------------------------------------------------------------------------------------------------------" -ForegroundColor Green
-    Write-Host "[11] Microsoft Program Install                     [21] * Microsoft Windows Fixed *       " -ForegroundColor DarkGray                    
+    Write-Host "[11] Microsoft Program Install                     [21] Microsoft Windows Fixed *         " -ForegroundColor DarkGray                    
     Write-Host "[12] Microsoft .NET Install                                                               " -ForegroundColor DarkGray
     Write-Host "------------------------------------------------------------------------------------------------------------------------" -ForegroundColor Green
-    Write-Host "[31] Setup Program Install ISO + EXE               [41] * Winget Install *                " -ForegroundColor DarkGray
-    Write-Host "[32] Setup Microsoft Office Install EXE            [42] * Chocolatey Install *            " -ForegroundColor DarkGray
+    Write-Host "[31] Setup Program Install ISO + EXE               [41] Winget Install *                  " -ForegroundColor DarkGray
+    Write-Host "[32] Setup Microsoft Office Install EXE            [42] Chocolatey Install *              " -ForegroundColor DarkGray
     Write-Host "------------------------------------------------------------------------------------------------------------------------" -ForegroundColor Green
-    Write-Host "[90] *** Standard PC Install All in One ***        [91] Winget + Chocolatey Upgrade       " -ForegroundColor DarkGray
+    Write-Host "[90] *Standard PC Install All in One               [91] Winget + Chocolatey Upgrade       " -ForegroundColor DarkGray
     Write-Host "------------------------------------------------------------------------------------------------------------------------" -ForegroundColor Green
     Write-Host "[98] Windows Utility (winutil)                     [99] Microsoft Activation Scripts (MAS)" -ForegroundColor DarkGray
     Write-Host "------------------------------------------------------------------------------------------------------------------------" -ForegroundColor Green
