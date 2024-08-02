3bf966baed2a39dcff24c64d636b75bf581c2358
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-02 21:52:21 +0300

    Update menu11.ps1

--- menu11.ps1
+++ menu11.ps1
@@ -1,7 +1,7 @@
 function Show-Menu {
     cls
     Write-Host "-------------------------------------------------------------------"
-    Write-Host "--- menu_11 --- Microsoft Program Install ---                   ---"
+    Write-Host "--- menu_11 --- Microsoft Program Install ---                   ---" -ForegroundColor Red
     Write-Host "-------------------------------------------------------------------"
     Write-Host "[1]  Microsoft OneNote Store"
     Write-Host "[2]  Microsoft PC Manager"
@@ -16,7 +16,7 @@ function Show-Menu {
     Write-Host "[11] Microsoft 365 Apps for enterprise"
     Write-Host "[12] Windows Scan"
     Write-Host "-------------------------------------------------------------------"
-    Write-Host "[0] Exit"
+    Write-Host "[0] Exit" -ForegroundColor Red
     Write-Host "-------------------------------------------------------------------"
 }
 
