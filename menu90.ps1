21f012b38198ac467562e3254d2288d1300ffa0c
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-03 10:08:04 +0300

    Update menu90.ps1

--- menu90.ps1
+++ menu90.ps1
@@ -1,5 +1,5 @@
-# showmenu
-function ShowMenu {
+# Function to display the menu
+function Show-Menu {
     Clear-Host
     Write-Output "-------------------------------------------------------------------"
     Write-Output "--- menu_21    --- DEFAULT PC INSTALLER ---                     ---" -ForegroundColor Red
@@ -11,29 +11,49 @@ function ShowMenu {
     Write-Output "-------------------------------------------------------------------"
 }
 
-# Display the menu first
-ShowMenu
-
-# Get user choice
-$choice = Read-Host "Enter your choice (0,1,2,3...)"
-
-
-# Handle user choices
-switch ($choice) {
-    '1' {
-        Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu90_1.ps1").Content
-    }
-    '2' {
-        Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu90_2.ps1").Content
-    }
-    '0' {
-        Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/main/program.ps1").Content
-    }
-    default {
-        Write-Output "Invalid choice. Please enter a valid option."
+# Function to handle user choice
+function Handle-Choice {
+    param (
+        [string]$choice
+    )
+    
+    switch ($choice) {
+        '1' {
+            try {
+                $content = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu90_1.ps1" -ErrorAction Stop
+                Invoke-Expression $content.Content
+            } catch {
+                Write-Output "Error fetching or executing script for USER DEFAULT SET."
+            }
+        }
+        '2' {
+            try {
+                $content = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu90_2.ps1" -ErrorAction Stop
+                Invoke-Expression $content.Content
+            } catch {
+                Write-Output "Error fetching or executing script for INSTALL DEFAULT."
+            }
+        }
+        '0' {
+            try {
+                $content = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/main/program.ps1" -ErrorAction Stop
+                Invoke-Expression $content.Content
+            } catch {
+                Write-Output "Error fetching or executing script for Exit."
+            }
+        }
+        default {
+            Write-Output "Invalid choice. Please enter a valid option."
+        }
     }
 }
 
-# Pause and return to menu
-Read-Host "Press Enter to continue..."
-ShowMenu
+# Main loop to display the menu and handle user choices
+do {
+    Show-Menu
+    $choice = Read-Host "Enter your choice (0,1,2,3...)"
+    Handle-Choice -choice $choice
+    if ($choice -ne '0') {
+        Read-Host "Press Enter to continue..."
+    }
+} while ($choice -ne '0')
