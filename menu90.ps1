a172621c72453f95054fa99a38969ba2267d4ff3
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-03 10:05:44 +0300

    Update menu90.ps1

--- menu90.ps1
+++ menu90.ps1
@@ -17,44 +17,14 @@ ShowMenu
 # Get user choice
 $choice = Read-Host "Enter your choice (0,1,2,3...)"
 
-# Define temporary directory
-$tempDir = "$env:Temp"
-
-function DownloadAndExecute {
-    param (
-        [string]$url,
-        [string]$fileName
-    )
-    
-    $filePath = Join-Path -Path $tempDir -ChildPath $fileName
-    
-    # Download the script
-    Invoke-WebRequest -Uri $url -OutFile $filePath
-    
-    # Execute the script if it was downloaded successfully
-    if (Test-Path $filePath) {
-        & $filePath
-        Remove-Item -Path $filePath
-    }
-}
 
 # Handle user choices
 switch ($choice) {
     '1' {
-        Write-Output "Downloading and executing menu21_1.cmd..."
-        DownloadAndExecute -url "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu90_1.cmd" -fileName "menu21_1.cmd"
+        Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu90_1.ps1").Content
     }
     '2' {
-        Write-Output "Downloading and executing menu21_2.cmd..."
-        DownloadAndExecute -url "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu90_2.cmd" -fileName "menu21_2.cmd"
-    }
-    '3' {
-        Write-Output "Downloading and executing menu21_3.cmd..."
-        DownloadAndExecute -url "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu90_3.cmd" -fileName "menu21_3.cmd"
-    }
-    '4' {
-        Write-Output "Downloading and executing menu21_4.cmd..."
-        DownloadAndExecute -url "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu90_4.cmd" -fileName "menu21_4.cmd"
+        Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu90_2.ps1").Content
     }
     '0' {
         Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/main/program.ps1").Content
