bc0ea7c343fff2e845a6b06c766924670497b9eb
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-03 11:26:28 +0300

    Update and rename microsoft_fixmenu.ps1 to fix.ps1

--- fix.ps1
+++ fix.ps1
@@ -22,10 +22,10 @@ function Handle-Choice {
 
 # Handle user choices
 switch ($choice) {
-    1 { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu21_1.ps1").Content }
-    2 { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu21_2.ps1").Content }
-    3 { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu21_3.ps1").Content }
-    4 { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/menu21_4.ps1").Content }
+    1 { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/fix1.ps1").Content }
+    2 { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/fix2.ps1").Content }
+    3 { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/fix3.ps1").Content }
+    4 { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/menu/fix4.ps1").Content }
     0 { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/main/program.ps1").Content }
     default {
         Write-Output "Invalid choice. Please enter a valid option."
