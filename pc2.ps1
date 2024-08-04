bbca382d673c124977430ef8196a45bb950302ce
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-04 23:21:28 +0300

    Update pc2.ps1

--- pc2.ps1
+++ pc2.ps1
@@ -91,37 +91,10 @@ if (Test-Path $adobePath) {
     Write-Host "Adobe Reader not found at $adobePath. Please make sure it is installed."
 }
 
-# Prompt user for Office setup
-$setupOffice = Read-Host "Do you want to set up OFFICE? (Yes(Y) / No(N))"
-if ($setupOffice -eq 'Y') {
-    Start-Process "\\192.168.18.2\setup\source\office\office2021ProPlus_tr\Setup64.exe"
-}
 
 # Pause to allow user to view the previous message
 Start-Sleep -Seconds 2
-Clear-Host
 
-Write-Host "------------------------"
-
-# Prompt user for Massgrave setup
-$masOpen = Read-Host "mas open or? (Yes(Y) / No(N))"
-if ($masOpen -eq 'Y') {
-    Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/emreuls7/mr.winls/tool/massgrave_mas.ps1' -OutFile "$env:TEMP\massgrave_mas.ps1"
-    & "$env:TEMP\massgrave_mas.ps1"
-}
-
-Write-Host "--------------------------------------"
-Write-Host "--- BUNUDA BITIRDIN HADI IYISIN... ---"
-Write-Host "--------------------------------------"
-
-# Pause to allow user to view the previous message
-Read-Host -Prompt "Press Enter to continue"
-
-Clear-Host
-
-Write-Host "--------------------------------"
-Write-Host "---SEN GEDE RESTART AT AGA...---"
-Write-Host "--------------------------------"
 
 # Prompt user for restart
 # Uncomment if restart is desired
