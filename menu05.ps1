66e2a3d7120885b85a501b8b814d462cc8148892
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-05 01:16:49 +0300

    Update menu05.ps1

--- menu05.ps1
+++ menu05.ps1
@@ -26,13 +26,29 @@ function Install-Or-Upgrade {
 }
 
 do {
-    Show-Menu
-    $choice = Read-Host "Enter your choice (0,1,2...)"
-    switch ($choice) {
-        "1" { Install-Or-Upgrade -id "9NVMNJCR03XV" } # MSI Center
-        "2" { Install-Or-Upgrade -id "9P9WDH947752" } # MSI Driver App Center
-        "0" { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/main/program.ps1").Content }
-        default { Write-Host "Invalid choice. Please enter a number between 0 and 2." }
-    }
-} while ($choice -ne "0")
+   
+# Function to handle installations and activations
+function Handle-Choice {
+    param (
+        [int]$choice
+    )
 
+    switch ($choice) {
+        1 { Install-Or-Upgrade -id "9NVMNJCR03XV" } # MSI Center
+        2 { Install-Or-Upgrade -id "9P9WDH947752" } # MSI Driver App Center
+        0 {
+            exit
+            # Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/main/program.ps1").Content
+        }
+        default { Write-Host "Invalid choice. Please try again." }
+    }
+}
+
+# Main script loop
+do {
+    Show-Menu
+    $choice = Read-Host "Enter your choice (0,1,2,3...)"
+    Clear-Host
+    Handle-Choice -choice $choice
+    if ($choice -ne 0) { Start-Sleep -Seconds 2 }
+} while ($choice -ne 0)
