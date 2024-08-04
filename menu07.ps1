cce9ffe386806b6277e6bfe105f84254115b8e38
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-05 01:19:55 +0300

    Update menu07.ps1

--- menu07.ps1
+++ menu07.ps1
@@ -35,30 +35,45 @@ function Install-Software {
     winget install --id $id -e --accept-package-agreements --accept-source-agreements --silent
 }
 
+# Function to handle installations and activations
+function Handle-Choice {
+    param (
+        [int]$choice
+    )
+
+    switch ($choice) {
+        1 { Install-Software -id "Fortinet.FortiClientVPN" }
+        2 { Install-Software -id "Windscribe.Windscribe" }
+        3 { Install-Software -id "ExpressVPN.ExpressVPN" }
+        4 { Install-Software -id "NordVPN.NordVPN" }
+        5 { Install-Software -id "ProtonTechnologies.ProtonVPN" }
+        6 { Install-Software -id "Radmin.VPN" }
+        7 { Install-Software -id "OpenVPNTechnologies.OpenVPN" }
+        8 { Install-Software -id "OpenVPNTechnologies.OpenVPNConnect" }
+        9 { Install-Software -id "WireGuard.WireGuard" }
+        10 { Install-Software -id "MullvadVPN.MullvadVPN" }
+        11 { Install-Software -id "uvncbvba.UltraVnc" }
+        12 { Install-Software -id "RealVNC.VNCViewer" }
+        13 { Install-Software -id "RealVNC.VNCServer" }
+        14 { Install-Software -id "AnyDeskSoftwareGmbH.AnyDesk" }
+        15 { Install-Software -id "TeamViewer.TeamViewer" }
+        16 { Install-Software -id "Google.ChromeRemoteDesktopHost" }
+        21 { Install-Software -id "mRemoteNG.mRemoteNG" }
+        22 { Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "winget install --id 9PNMNF92JNFP --accept-package-agreements --accept-source-agreements --silent"' -Wait }
+        0 {
+            exit
+            # Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/main/program.ps1").Content
+        }
+        default { Write-Host "Invalid choice. Please try again." }
+    }
+}
+
+# Main script loop
 do {
     Show-Menu
     $choice = Read-Host "Enter your choice (0,1,2,3...)"
-    switch ($choice) {
-        "1" { Install-Software -id "Fortinet.FortiClientVPN" }
-        "2" { Install-Software -id "Windscribe.Windscribe" }
-        "3" { Install-Software -id "ExpressVPN.ExpressVPN" }
-        "4" { Install-Software -id "NordVPN.NordVPN" }
-        "5" { Install-Software -id "ProtonTechnologies.ProtonVPN" }
-        "6" { Install-Software -id "Radmin.VPN" }
-        "7" { Install-Software -id "OpenVPNTechnologies.OpenVPN" }
-        "8" { Install-Software -id "OpenVPNTechnologies.OpenVPNConnect" }
-        "9" { Install-Software -id "WireGuard.WireGuard" }
-        "10" { Install-Software -id "MullvadVPN.MullvadVPN" }
-        "11" { Install-Software -id "uvncbvba.UltraVnc" }
-        "12" { Install-Software -id "RealVNC.VNCViewer" }
-        "13" { Install-Software -id "RealVNC.VNCServer" }
-        "14" { Install-Software -id "AnyDeskSoftwareGmbH.AnyDesk" }
-        "15" { Install-Software -id "TeamViewer.TeamViewer" }
-        "16" { Install-Software -id "Google.ChromeRemoteDesktopHost" }
-        "21" { Install-Software -id "mRemoteNG.mRemoteNG" }
-        "22" { Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "winget install --id 9PNMNF92JNFP --accept-package-agreements --accept-source-agreements --silent"' -Wait }
-        "0" { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/main/program.ps1").Content }
-        default { Write-Host "Invalid choice. Please enter a number between 0 and 22." }
-    }
-} while ($choice -ne "0")
+    Clear-Host
+    Handle-Choice -choice $choice
+    if ($choice -ne 0) { Start-Sleep -Seconds 2 }
+} while ($choice -ne 0)
 
