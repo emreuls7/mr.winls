26235530f846db0bfdf64d6c2a5220a47bd61a9a
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-05 11:21:17 +0300

    Update pasha_tiger.ps1

--- pasha_tiger.ps1
+++ pasha_tiger.ps1
@@ -1,92 +1,118 @@
-# Menü işlevini tanımlar
-function Show-Menu {
-    cls
-    Write-Host "------------------------------------------------"
-    Write-Host "---    Pasha Lefkosa Tiger3 Install          ---"
-    Write-Host "------------------------------------------------"
-    Write-Host "[1] Windows Credentials 192.168.13.3 add"
-    Write-Host "[2] Tiger3 PASHA Copy C"
-    Write-Host "[3] Tiger3 2015-2019 Copy C"
-    Write-Host "[4] Tiger3 2019 Copy C"
-    Write-Host "[5] Tiger3 2022 Copy C"
-    Write-Host "------------------------------------------------"
-    Write-Host "[6] Windows Credentials 192.168.48.4 add"
-    Write-Host "[7] Tiger3 AKUN Copy C"
-    Write-Host "------------------------------------------------"
-    Write-Host "[11] License Tiger3 PASHA"
-    Write-Host "[12] License Tiger3 2015-2019"
-    Write-Host "[13] License Tiger3 2019"
-    Write-Host "[14] License Tiger3 2022"
-    Write-Host "------------------------------------------------"
-    Write-Host "[15] License Tiger3 AKUN"
-    Write-Host "[21] LHOSVR Windows host"
-    Write-Host "[0] Exit" -ForegroundColor Red
-    Write-Host "------------------------------------------------"
-}
+# Clear the console
+Clear-Host
 
-# Kullanıcıdan seçim almak için işlev
-function Get-UserChoice {
+function Show-Menu {
+    Clear-Host
+    Write-Host "------------------------------------------------" -ForegroundColor Cyan
+    Write-Host "---    Pasha Lefkosa Tiger3 Install          ---" -ForegroundColor Cyan
+    Write-Host "------------------------------------------------" -ForegroundColor Cyan
+    Write-Host "[1] Windows Credentials 192.168.13.3 add" -ForegroundColor Yellow
+    Write-Host "[2] Tiger3 PASHA Copy C" -ForegroundColor Yellow
+    Write-Host "[3] Tiger3 2015-2019 Copy C" -ForegroundColor Yellow
+    Write-Host "[4] Tiger3 2019 Copy C" -ForegroundColor Yellow
+    Write-Host "[5] Tiger3 2022 Copy C" -ForegroundColor Yellow
+    Write-Host "------------------------------------------------" -ForegroundColor Cyan
+    Write-Host "[6] Windows Credentials 192.168.48.4 add" -ForegroundColor Yellow
+    Write-Host "[7] Tiger3 AKUN Copy C" -ForegroundColor Yellow
+    Write-Host "------------------------------------------------" -ForegroundColor Cyan
+    Write-Host "[11] License Tiger3 PASHA" -ForegroundColor Yellow
+    Write-Host "[12] License Tiger3 2015-2019" -ForegroundColor Yellow
+    Write-Host "[13] License Tiger3 2019" -ForegroundColor Yellow
+    Write-Host "[14] License Tiger3 2022" -ForegroundColor Yellow
+    Write-Host "------------------------------------------------" -ForegroundColor Cyan
+    Write-Host "[15] License Tiger3 AKUN" -ForegroundColor Yellow
+    Write-Host "------------------------------------------------" -ForegroundColor Cyan
+    Write-Host "[21] LHOSVR Windows host" -ForegroundColor Yellow
+    Write-Host "------------------------------------------------" -ForegroundColor Cyan
+    Write-Host "[0] Exit" -ForegroundColor Yellow
+    Write-Host "------------------------------------------------" -ForegroundColor Cyan
+
     $choice = Read-Host "Enter your choice (0,1,2,3...)"
     return $choice
 }
 
-# Her seçimden sonra kullanıcıyı duraklatmak için işlev
-function Pause-Screen {
-    Read-Host "Press Enter to continue..."
+function CmdKey-Add-192_168_13_3 {
+    cmdkey /add:192.168.13.3 /user:grandpasha /pass:grand2013
+    Write-Host "Windows Credentials for 192.168.13.3 added."
 }
 
-# Windows Credentials ekleme
-function Add-Credentials {
-    param (
-        [string]$server,
-        [string]$user,
-        [string]$password
-    )
-    cmdkey /add:$server /user:$user /pass:$password
-    Write-Host "Windows Credentials for $server added."
+function CmdKey-Add-192_168_48_4 {
+    cmdkey /add:192.168.48.4 /user:grandpasha /pass:grand2013
+    Write-Host "Windows Credentials for 192.168.48.4 added."
 }
 
-# Dosya kopyalama işlemleri
-function Copy-Files {
-    param (
-        [string]$source,
-        [string]$destination
-    )
-    robocopy $source $destination /E /IS
-    Write-Host "Files copied from $source to $destination."
+function Copy-Tiger3-Pasha {
+    robocopy "\\192.168.13.3\tiger3_share\TIGER3_PASHA" "C:\TIGER3\TIGER3_PASHA" /E /IS
+    Write-Host "Tiger3 PASHA files copied to C:\TIGER3\TIGER3_PASHA."
 }
 
-# License Dosyası Kopyalama
-function Copy-License {
-    param (
-        [string]$source,
-        [string]$destination
-    )
-    robocopy $source $destination License.Dat /IS
+function Copy-Tiger3-2015_2019 {
+    robocopy "\\192.168.13.3\tiger3_share\TIGER3_2015-2019" "C:\TIGER3\TIGER3_2015-2019" /E /IS
+    Write-Host "Tiger3 2015-2019 files copied to C:\TIGER3\TIGER3_2015-2019."
+}
+
+function Copy-Tiger3-2019 {
+    robocopy "\\192.168.13.3\tiger3_share\TIGER3_2019" "C:\TIGER3\TIGER3_2019" /E /IS
+    Write-Host "Tiger3 2019 files copied to C:\TIGER3\TIGER3_2019."
+}
+
+function Copy-Tiger3-2022 {
+    robocopy "\\192.168.13.3\tiger3_share\TIGER3_2022" "C:\TIGER3\TIGER3_2022" /E /IS
+    Write-Host "Tiger3 2022 files copied to C:\TIGER3\TIGER3_2022."
+}
+
+function Copy-Tiger3-Akun {
+    robocopy "\\192.168.48.4\tiger3_share\TIGER3_AKUN" "C:\TIGER3\TIGER3_AKUN" /E /IS
+    Write-Host "Tiger3 AKUN files copied to C:\TIGER3\TIGER3_AKUN."
+}
+
+function License-Tiger3-Pasha {
+    robocopy "\\192.168.13.3\tiger3_share\TIGER3_PASHA" "C:\TIGER3\TIGER3_PASHA" License.Dat /IS
     Write-Host "*AFFERIM LISANSI YAPTIN TEBRIK EDRIM ..."
 }
 
-# Ana menüyü döngü içinde çalıştırır
-do {
-    Show-Menu
-    $choice = Get-UserChoice
+function License-Tiger3-2015_2019 {
+    robocopy "\\192.168.13.3\tiger3_share\TIGER3_2015-2019" "C:\TIGER3\TIGER3_2015-2019" License.Dat /IS
+    Write-Host "*AFFERIM LISANSI YAPTIN TEBRIK EDRIM ..."
+}
 
+function License-Tiger3-2019 {
+    robocopy "\\192.168.13.3\tiger3_share\TIGER3_2019" "C:\TIGER3\TIGER3_2019" License.Dat /IS
+    Write-Host "*AFFERIM LISANSI YAPTIN TEBRIK EDRIM ..."
+}
+
+function License-Tiger3-2022 {
+    robocopy "\\192.168.13.3\tiger3_share\TIGER3_2022" "C:\TIGER3\TIGER3_2022" License.Dat /IS
+    Write-Host "*AFFERIM LISANSI YAPTIN TEBRIK EDRIM ..."
+}
+
+function License-Tiger3-Akun {
+    robocopy "\\192.168.48.4\tiger3_share\TIGER3_AKUN" "C:\TIGER3\TIGER3_AKUN" License.Dat /IS
+    Write-Host "*AFFERIM LISANSI YAPTIN TEBRIK EDRIM ..."
+}
+
+function Run-Lhosvr {
+    Start-Process "\\192.168.13.3\muhasebe\License\TIGER3_HOST" -Wait
+}
+
+do {
+    $choice = Show-Menu
+    
     switch ($choice) {
-        "1" { Add-Credentials -server "192.168.13.3" -user "grandpasha" -password "grand2013" }
-        "2" { Copy-Files -source "\\192.168.13.3\tiger3_share\TIGER3_PASHA" -destination "C:\TIGER3\TIGER3_PASHA" }
-        "3" { Copy-Files -source "\\192.168.13.3\tiger3_share\TIGER3_2015-2019" -destination "C:\TIGER3\TIGER3_2015-2019" }
-        "4" { Copy-Files -source "\\192.168.13.3\tiger3_share\TIGER3_2019" -destination "C:\TIGER3\TIGER3_2019" }
-        "5" { Copy-Files -source "\\192.168.13.3\tiger3_share\TIGER3_2022" -destination "C:\TIGER3\TIGER3_2022" }
-        "6" { Add-Credentials -server "192.168.48.4" -user "grandpasha" -password "grand2013" }
-        "7" { Copy-Files -source "\\192.168.48.4\tiger3_share\TIGER3_AKUN" -destination "C:\TIGER3\TIGER3_AKUN" }
-        "11" { Copy-License -source "\\192.168.13.3\tiger3_share\TIGER3_PASHA" -destination "C:\TIGER3\TIGER3_PASHA" }
-        "12" { Copy-License -source "\\192.168.13.3\tiger3_share\TIGER3_2015-2019" -destination "C:\TIGER3\TIGER3_2015-2019" }
-        "13" { Copy-License -source "\\192.168.13.3\tiger3_share\TIGER3_2019" -destination "C:\TIGER3\TIGER3_2019" }
-        "14" { Copy-License -source "\\192.168.13.3\tiger3_share\TIGER3_2022" -destination "C:\TIGER3\TIGER3_2022" }
-        "15" { Copy-License -source "\\192.168.48.4\tiger3_share\TIGER3_AKUN" -destination "C:\TIGER3\TIGER3_AKUN" }
-        "21" { Start-Process "\\192.168.13.3\muhasebe\License\TIGER3_HOST" -Wait }
-        "0" { Write-Host "Goodbye!" -ForegroundColor Red; exit }
-        default { Write-Host "Invalid choice. Please select a valid option." -ForegroundColor Red }
+        "1" { CmdKey-Add-192_168_13_3 }
+        "2" { Copy-Tiger3-Pasha }
+        "3" { Copy-Tiger3-2015_2019 }
+        "4" { Copy-Tiger3-2019 }
+        "5" { Copy-Tiger3-2022 }
+        "6" { CmdKey-Add-192_168_48_4 }
+        "7" { Copy-Tiger3-Akun }
+        "11" { License-Tiger3-Pasha }
+        "12" { License-Tiger3-2015_2019 }
+        "13" { License-Tiger3-2019 }
+        "14" { License-Tiger3-2022 }
+        "15" { License-Tiger3-Akun }
+        "21" { Run-Lhosvr }
+        "0" { Write-Host "BYE"; Start-Sleep -Seconds 3; exit }
+        default { Write-Host "Invalid choice. Please select a valid option."; Start-Sleep -Seconds 2 }
     }
-    Pause-Screen
-} while ($true)
+} while ($choice -ne "0")
