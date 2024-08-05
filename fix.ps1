99bb7c6356d34c4bbb22a76aadb16f98e100d9af
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-05 10:31:52 +0300

    Update fix.ps1

--- fix.ps1
+++ fix.ps1
@@ -13,6 +13,31 @@ function Show-Menu {
     Write-Host "-------------------------------------------------------------------"
 }
 
+function Download-And-Execute-Script {
+    param (
+        [string]$Url
+    )
+    
+    # Geçici dosya yolunu oluştur
+    $tempFile = [System.IO.Path]::Combine("C:\Windows\Temp", [System.IO.Path]::GetRandomFileName() + ".ps1")
+    
+    try {
+        Write-Host "Downloading script from $Url..." -ForegroundColor Green
+        Invoke-WebRequest -Uri $Url -OutFile $tempFile
+        Write-Host "Script downloaded to $tempFile" -ForegroundColor Green
+        
+        Write-Host "Executing script..." -ForegroundColor Green
+        Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$tempFile`"" -Wait -NoNewWindow
+    } catch {
+        Write-Host "An error occurred while processing the script: $_" -ForegroundColor Red
+    } finally {
+        if (Test-Path $tempFile) {
+            Remove-Item $tempFile -Force
+            Write-Host "Temporary file removed." -ForegroundColor Green
+        }
+    }
+}
+
 # Kullanıcı seçimlerini işleyen fonksiyon
 function Handle-Choice {
     param (
@@ -24,34 +49,31 @@ function Handle-Choice {
 
     # Kullanıcı seçimlerini işleme
     switch ($choice) {
-        1 { $scriptUrl = "${baseUrl}fix1.ps1" }
-        2 { $scriptUrl = "${baseUrl}fix2.ps1" }
-        3 { $scriptUrl = "${baseUrl}fix3.ps1" }
-        4 { $scriptUrl = "${baseUrl}fix4.ps1" }
-        0 { $scriptUrl = "https://raw.githubusercontent.com/emreuls7/mr.winls/main/program.ps1" }
-        default {
-            Write-Host "Geçersiz seçim. Lütfen geçerli bir seçenek girin."
-            return
+        1 { Download-And-Execute-Script "$baseUrl/fix1.ps1" }
+        2 { Download-And-Execute-Script "$baseUrl/fix2.ps1" }
+        3 { Download-And-Execute-Script "$baseUrl/fix3.ps1" }
+        4 { Download-And-Execute-Script "$baseUrl/fix4.ps1" }
+         0 {
+            exit
+            # Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/main/program.ps1").Content
         }
-    }
-
-    # Script'i alıp çalıştırma
-    try {
-        $scriptContent = Invoke-WebRequest -Uri $scriptUrl -ErrorAction Stop
-        Invoke-Expression $scriptContent.Content
-        Write-Host "Script başarıyla çalıştırıldı."
-    } catch {
-        Write-Host "Script'i almak veya çalıştırmak başarısız oldu. Lütfen daha sonra tekrar deneyin."
+        default { Write-Host "Invalid choice. Please try again." }
     }
 }
 
-# Ana betik döngüsü
+# Main script loop
 do {
     Show-Menu
-    $choice = Read-Host "Seçiminizi girin (0,1,2,3...)"
+    $choice = Read-Host "Enter your choice (0,1,2,3...)"
     Clear-Host
     Handle-Choice -choice $choice
-    if ($choice -ne 0) { 
-        Start-Sleep -Seconds 2 
-    }
-} while ($choice -ne 0)
+
+    # Pause for 2 seconds if the choice is not '0'
+    #if ($choice -ne '0') { 
+     #   Start-Sleep -Seconds 2 
+    #}
+
+    # Prompt the user to press Enter to continue
+    Write-Host "`nPress Enter to continue..."
+    Read-Host
+} while ($choice -ne '0')
