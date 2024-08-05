1c4dd43a868fbe84cac152dc11e3a94a204cd9c8
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-05 15:00:25 +0300

    Update pasha_menu.ps1

--- pasha_menu.ps1
+++ pasha_menu.ps1
@@ -5,8 +5,8 @@ function Show-Menu {
     Write-Host "-------------------------------------------------------------------" -ForegroundColor Green
     Write-Host "---                  --- PASHA LEFKOSA ---                      ---" -ForegroundColor Red
     Write-Host "-------------------------------------------------------------------" -ForegroundColor Green
-    Write-Host "[1] TIGER3 LOGO" -ForegroundColor DarkRed
-    Write-Host "[2] MC FIDELIO" -ForegroundColor DarkBlue
+    Write-Host "[1] TIGER3 LOGO            " -ForegroundColor DarkMagenta
+    Write-Host "[2] MC FIDELIO                " -ForegroundColor DarkCyan
     Write-Host "-------------------------------------------------------------------" -ForegroundColor Green
     Write-Host "[0] Exit                                                           " -ForegroundColor Red
     Write-Host "-------------------------------------------------------------------" -ForegroundColor Green
@@ -45,8 +45,8 @@ function Handle-Choice {
     
     Clear-Host
     switch ($Choice) {
-        1 { Download-And-Execute-Script "https://raw.githubusercontent.com/emreuls7/mr.winls/pasha//pasha_tiger.ps1" }
-        2 { Download-And-Execute-Script "https://raw.githubusercontent.com/emreuls7/mr.winls/pasha//pasha_mcfid.ps1" }
+        1 { Download-And-Execute-Script "https://raw.githubusercontent.com/emreuls7/mr.winls/pasha/pasha_tiger.ps1" }
+        2 { Download-And-Execute-Script "https://raw.githubusercontent.com/emreuls7/mr.winls/pasha/pasha_mcfid.ps1" }
         0 {
             exit
             # Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/main/program.ps1").Content
