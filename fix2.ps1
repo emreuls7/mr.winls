250e0fc957c535fc8842604fa6b464e8e3a10a22
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-05 10:35:10 +0300

    Update fix2.ps1

--- fix2.ps1
+++ fix2.ps1
@@ -17,45 +17,46 @@ function Show-Menu_21_2 {
     Write-Host "-------------------------------------------------------------------"
 }
 
-# Main loop to handle menu choices
-do {
-    Show-Menu_21_2
-    $choice = Read-Host "Enter your choice (0,1,2,3,4,5,6,7,8)"
+# Function to handle installations and activations
+function Handle-Choice {
+    param (
+        [int]$choice
+    )
 
     switch ($choice) {
-        '1' {
+        1 {
             Write-Host "Disabling User Account Control..."
             Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value 0 -Type DWord
             Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 0 -Type DWord
             Write-Host "User Account Control disabled. You need to restart your computer for changes to take effect."
             Pause
         }
-        '2' {
+        2 {
             Write-Host "Enabling User Account Control..."
             Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value 1 -Type DWord
             Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 5 -Type DWord
             Write-Host "User Account Control enabled. You need to restart your computer for changes to take effect."
             Pause
         }
-        '3' {
+        3 {
             Write-Host "Turning off Windows Defender Firewall..."
             netsh advfirewall set allprofiles state off
             Write-Host "Windows Defender Firewall turned off."
             Pause
         }
-        '4' {
+        4 {
             Write-Host "Turning on Windows Defender Firewall..."
             netsh advfirewall set allprofiles state on
             Write-Host "Windows Defender Firewall turned on."
             Pause
         }
-        '5' {
+        5 {
             Write-Host "Fixing Network Folder settings..."
             Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters" -Name "AllowInsecureGuestAuth" -Value 1 -Type DWord
             Write-Host "Network Folder settings updated."
             Pause
         }
-        '6' {
+        6 {
             Write-Host "Enabling Network Discovery..."
             netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes
             netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=Yes
@@ -70,27 +71,39 @@ do {
             Write-Host "Network Discovery enabled."
             Pause
         }
-        '7' {
+        7 {
             Write-Host "Removing 'Open File - Security Warning' Prompt..."
             Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Associations" -Name "LowRiskFileTypes" -Value ".exe;.bat;.reg;.vbs;.js;.msi" -Type String
             Write-Host "'Open File - Security Warning' Prompt removed."
             Pause
         }
-        '8' {
+        8 {
             Write-Host "Restoring 'Open File - Security Warning' Prompt..."
             Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Associations" -Name "LowRiskFileTypes" -ErrorAction SilentlyContinue
             Write-Host "'Open File - Security Warning' Prompt restored."
             Pause
         }
-        '0' {
+        0 {
+            # exit
             Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/win/fix.ps1").Content
-            }
-            exit
         }
-        default {
-            Write-Host "Invalid choice. Please enter a valid option."
-            Pause
-        }
+        default { Write-Host "Invalid choice. Please try again." }
     }
-} while ($true)
+}
 
+# Main script loop
+do {
+    Show-Menu
+    $choice = Read-Host "Enter your choice (0,1,2,3...)"
+    Clear-Host
+    Handle-Choice -choice $choice
+
+    # Pause for 2 seconds if the choice is not '0'
+    #if ($choice -ne '0') { 
+     #   Start-Sleep -Seconds 2 
+    #}
+
+    # Prompt the user to press Enter to continue
+    Write-Host "`nPress Enter to continue..."
+    Read-Host
+} while ($choice -ne '0')
