4d52a32129717d387b4b66b451e445feb7e09b92
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-05 15:08:54 +0300

    Update ps3.ps1

--- ps3.ps1
+++ ps3.ps1
@@ -6,34 +6,6 @@ Write-Host "`nPress Enter to continue..."
 
 Clear-Host
 
-
-function Download-And-Execute-Script {
-    param (
-        [string]$Url
-    )
-    
-    # Geçici dosya yolunu oluştur
-    $tempFile = [System.IO.Path]::Combine("C:\Windows\Temp", [System.IO.Path]::GetRandomFileName() + ".ps1")
-    
-    try {
-        Write-Host "Downloading script from $Url..." -ForegroundColor Green
-        Invoke-WebRequest -Uri $Url -OutFile $tempFile
-        Write-Host "Script downloaded to $tempFile" -ForegroundColor Green
-        
-        Write-Host "Executing script..." -ForegroundColor Green
-        Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$tempFile`"" -Wait -NoNewWindow
-    } catch {
-        Write-Host "An error occurred while processing the script: $_" -ForegroundColor Red
-    } finally {
-        if (Test-Path $tempFile) {
-            Remove-Item $tempFile -Force
-            Write-Host "Temporary file removed." -ForegroundColor Green
-        }
-    }
-}
-     # Script kaynakları için temel URL
-    $baseUrl = "https://raw.githubusercontent.com/emreuls7/mr.winls/tool/"
-
     
 # Start Office setup and handle any errors
 try {
@@ -43,9 +15,7 @@ try {
 catch {
     Write-Host "Failed to start Office setup. The file may not be found. Continuing with the script..."
 }
-
 Read-Host -Prompt "Press Enter to continue after Office setup has started or if it failed"
-
 # Pause to allow user to view the previous message
 Start-Sleep -Seconds 2
 Clear-Host
@@ -58,7 +28,6 @@ try {
 catch {
     Write-Host "Failed to execute Massgrave setup script. Continuing with the script..."
 }
-
 # Notify user that script execution is complete and wait for Enter key
 Write-Host "Script execution completed. Press Enter to exit."
 Read-Host
