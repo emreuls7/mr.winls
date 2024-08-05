eaeba2325d2bd7716ee7cfbdf4fa4f9884c5de3a
emreuls7 <76229459+emreuls7@users.noreply.github.com>
2024-08-05 10:42:06 +0300

    Update microsoft1.ps1

--- microsoft1.ps1
+++ microsoft1.ps1
@@ -40,13 +40,13 @@ do {
     Show-Menu
     $choice = Read-Host "Enter your choice (0,1,2,3...)"
     switch ($choice) {
-        "1" { Install-Software -id "XPFFZHVGQWWLHB" }
-        "2" { Install-Software -id "Microsoft.PCManager" }
-        "3" { Install-Software -id "Microsoft.OneDrive" }
-        "4" { Install-Software -id "Microsoft.Teams" }
-        "5" { Install-Software -id "Microsoft.PowerToys" }
-        "6" { Install-Software -id "Microsoft.DirectX" }
-        "7" {
+        1 { Install-Software -id "XPFFZHVGQWWLHB" }
+        2 { Install-Software -id "Microsoft.PCManager" }
+        3 { Install-Software -id "Microsoft.OneDrive" }
+        4 { Install-Software -id "Microsoft.Teams" }
+        5 { Install-Software -id "Microsoft.PowerToys" }
+        6 { Install-Software -id "Microsoft.DirectX" }
+        7 {
             $vcPackages = @(
                 "Microsoft.VCRedist.2005.x86",
                 "Microsoft.VCRedist.2005.x64",
@@ -63,7 +63,7 @@ do {
             )
             Install-Software -id $vcPackages
         }
-        "8" {
+        8 {
             $dotnetPackages = @(
                 "dotnet3.5",
                 "dotnet4.5.1",
@@ -71,12 +71,32 @@ do {
             )
             Install-ChocoSoftware -packages $dotnetPackages
         }
-        "9" { Install-Software -id "Grammarly.Grammarly" }
-        "10" { Install-Software -id "9WZDNCRFJ3PS" }
-        "11" { Install-Software -id "Microsoft.Office" }
-        "12" { Install-Software -id "9WZDNCRFJ3PV" }
-        "0" { Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/win/microsoft_menu.ps1").Content }
-        default { Write-Host "Invalid choice. Please enter a number between 0 and 12." }
+        9 { Install-Software -id "Grammarly.Grammarly" }
+        10 { Install-Software -id "9WZDNCRFJ3PS" }
+        11 { Install-Software -id "Microsoft.Office" }
+        12 { Install-Software -id "9WZDNCRFJ3PV" }
+        0 {
+            exit
+            # Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/emreuls7/mr.winls/win/microsoft_menu.ps1").Content
+        }
+        default { Write-Host "Invalid choice. Please try again." }
     }
-} while ($choice -ne "0")
+}
+
+# Main script loop
+do {
+    Show-Menu
+    $choice = Read-Host "Enter your choice (0,1,2,3...)"
+    Clear-Host
+    Handle-Choice -choice $choice
+
+    # Pause for 2 seconds if the choice is not '0'
+    #if ($choice -ne '0') { 
+     #   Start-Sleep -Seconds 2 
+    #}
+
+    # Prompt the user to press Enter to continue
+    Write-Host "`nPress Enter to continue..."
+    Read-Host
+} while ($choice -ne '0')
 
